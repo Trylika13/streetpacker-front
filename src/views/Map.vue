add2a5d01ba11513c3b56e3ee1c16da1a77b012c
jeangreg <jean.g.apprenant@gmail.com>
2026-05-06 14:18:45 +0200

    streetpacker-front-4 Intégration de MapTiler

---
+++ src/views/Map.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="map-wrap">
+    <div ref="mapContainer" class="map"></div>
+  </div>
+</template>
+
+<script setup>
+import { ref, shallowRef, onMounted, onUnmounted } from 'vue';
+import { config, Map, MapStyle } from '@maptiler/sdk';
+import '@maptiler/sdk/dist/maptiler-sdk.css';
+
+const mapContainer = ref(null);
+const map = shallowRef(null);
+onMounted(() => {
+  config.apiKey = '4FOCYl0j1EWf7D5remSs';
+
+  if (mapContainer.value) {
+    map.value = new Map({
+      container: mapContainer.value,
+      style: MapStyle.BASE,
+      center: [5.694040, 50.679083], // Longitude, Latitude
+      zoom: 15
+    });
+  }
+});
+
+onUnmounted(() => {
+  if (map.value) {
+    map.value.remove();
+  }
+});
+</script>
+<style scoped>
+.map-wrap {
+  width: 100%;
+  height: 100vh;
+  background-color: #eee;
+}
+.map {
+  width: 100%;
+  height: 100%;
+}
+</style>
